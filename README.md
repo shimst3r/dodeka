3fdc6dabaf20da49353b7b3f2aa9df8b2e2c0d1d
Nils Müller <nils.mueller@grandcentrix.net>
2019-10-15 11:06:26 +0200

    Add initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dodeka
+
 dodeka is a command line tool for tracking twelve-factor app fulfilment
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Copyright (c) 2019, Nils P. Müller <shimst3r@gmail.com>
+All rights reserved.
+
+This file is part of dodeka.
+
+License: MIT, see https://opensource.org/licenses/MIT
+"""
+
+from setuptools import setup
+
+__version__ = "0.0.1"
+
+with open("README.md", "r", encoding="utf-8") as f_in:
+    LONG_DESCRIPTION = f_in.read()
+
+PACKAGES = ["dodeka"]
+
+with open("requirements.txt", "r", encoding="utf-8") as f_in:
+    # skip the first line which contains meta data
+    REQUIRES = [line.strip() for line in f_in.readlines()[1:]]
+
+setup(
+    author="Nils P. Müller",
+    author_email="shimst3r@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="Twelve-Factor App Checker",
+    install_requires=REQUIRES,
+    keywords=["cli", "tool", "twelvefactor"],
+    license="MIT",
+    long_description=LONG_DESCRIPTION,
+    name="dodeka",
+    packages=PACKAGES,
+    url="https://github.com/shimst3r/dodeka",
+    version=__version__,
+)
